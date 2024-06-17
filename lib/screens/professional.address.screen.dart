4a61b5cbc3bffa7d7db6cec82621ce40e9abf061
maherBoughdiri <mboughdiri4@gmail.com>
2024-06-17 16:24:28 +0200

    [TASK] enhanced on profissional address

--- lib/screens/professional.address.screen.dart
+++ lib/screens/professional.address.screen.dart
@@ -9,24 +9,44 @@ class ProfessionalAddressScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Professional Address'),
       ),
-      body: Column(children: [
-        Expanded(
-          child: FlutterMap(
-            options: MapOptions(
-              initialCenter: LatLng(51.5,
-                  -0.09), // Replace with your professional address coordinates
-              initialZoom: 13.0,
-            ),
-            children: [
-              TileLayer(
-                urlTemplate:
-                    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                subdomains: ['a', 'b', 'c'],
+      body: Column(
+        children: [
+          Expanded(
+            child: FlutterMap(
+              options: MapOptions(
+                initialCenter: LatLng(34.750918, 10.729254),
+                initialZoom: 13.0,
+                minZoom: 10.0,
               ),
-            ],
+              children: [
+                TileLayer(
+                  urlTemplate:
+                      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                  subdomains: ['a', 'b', 'c'],
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      width: 100.0,
+                      height: 136.0,
+                      point: LatLng(34.750918,
+                          10.729254), // Replace with your professional address coordinates
+                      child: Container(
+                        child: Image.asset(
+                          'assets/images/location_pin.png', // Replace with your asset path
+                          width: 404.0,
+                          height: 548.0,
+                        ),
+                      ),
+                      alignment: Alignment.topCenter,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }
